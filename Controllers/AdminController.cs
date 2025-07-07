74ba9397bd70b0b733db93034cde3f170a7c3298
Ramazan Koçak <ramazankocak@Ramazan-MacBook-Pro.local>
2025-07-07 17:04:24 +0300

    Blog List page

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Context;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly BlogDbContext _db;
+        public AdminController(BlogDbContext db)
+        {
+            _db = db;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BlogList()
+        {
+            var blogs = _db.Blogs.ToList();
+
+            return View(blogs);
+        }
+        public IActionResult EditBlog()
+        {
+            return View();
+        }
+    }
+}
